edd068f4127f959f4d20d0318486b9536e1787f9
Paweł Wojtkowiak <p.wojtkowiak@fibargroup.com>
2017-01-01 22:04:47 +0100

    Initial Commit

---
+++ Fridgey/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fridgey
+//
+//  Created by Paweł Wojtkowiak on 01.01.2017.
+//  Copyright © 2017 Paweł Wojtkowiak. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
